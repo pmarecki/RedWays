1876e29a921dc97ea309538bbf1a019847b97f10
Piotr Marecki <pmarecki@gmail.com>
2017-03-14 08:54:18 +0100

    Create nero_s_xor_sums.cc

---
+++ tmp/nero_s_xor_sums.cc
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+const int kN = 100000 + 5;
+int n;
+std::vector<int> edges[kN];
+int a[kN];
+int c[kN][2];
+
+long long dfs(int u, int fa, int bit)
+{
+    c[u][0] = c[u][1] = 0;
+    c[u][a[u] >> bit & 1] ++;
+    long long ret = c[u][1];
+    for (int v: edges[u]) if (v != fa) {
+        ret += dfs(v, u, bit);
+        for (int i = 0; i < 2; ++ i)
+            ret += c[u][i] * 1ll * c[v][i ^ 1];
+        for (int i = 0; i < 2; ++ i)
+            c[u][i] += c[v][i ^ (a[u] >> bit & 1)];
+    }
+    return ret;
+}
+
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++ i) scanf("%d", a + i);
+    for (int i = 0; i < n - 1; ++ i) {
+        int a, b;
+        scanf("%d%d", &a, &b); a --; b --;
+        edges[a].emplace_back(b);
+        edges[b].emplace_back(a);
+    }
+    long long result = 0;
+    for (int bit = 0; bit < 20; ++ bit) {
+        result += dfs(0, -1, bit) * (1 << bit);
+    }
+    std::cout << result << std::endl;
+}
